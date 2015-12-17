0bc58db99cd18a0028d480bac43920df8df00eb3
Cyriac Domini Thundathil <cyriacd@outlook.com>
2015-12-17 07:39:39 -0500

    removed useless javascript

--- js/all.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function () {
-if( $(window).scrollTop() > $('#cheader').height() && !($('#cmenu').hasClass('csticky'))){
-$('#cmenu').addClass('csticky');
-} else if ($(window).scrollTop() < $('#cheader').height()){
-$('#cmenu').removeClass('csticky');
-}
-console.log($(window).scrollTop());
-console.log($('#cheader').height());
-});
