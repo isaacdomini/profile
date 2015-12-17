a51313df9edcaa3c289a08f68da89cae765f7a5a
Cyriac Domini Thundathil <cyriacd@outlook.com>
2015-12-16 20:56:49 -0500

    removed materializecss and added sticky menu

--- js/all.js
+++ js/all.js
@@ -1,1 +1,9 @@
-//= require_tree .
+$(window).scroll(function () {
+if( $(window).scrollTop() > $('#cmenu').offset().top && !($('#cmenu').hasClass('csticky'))){
+$('#cmenu').addClass('csticky');
+} else if ($(window).scrollTop() < $('#cheader').height()){
+$('#cmenu').removeClass('csticky');
+}
+console.log($(window).scrollTop());
+console.log($('#cheader').height());
+});
