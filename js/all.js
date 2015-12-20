4ba1a83e5b35ecb898bf4cdd0bf49e820ea1168a
Cyriac Domini Thundathil <cyriacd@outlook.com>
2015-12-19 22:31:12 -0500

    finished hamburger button actions

--- js/all.js
+++ js/all.js
@@ -1,3 +1,22 @@
+(function() {
+
+  "use strict";
+
+  var toggles = document.querySelectorAll(".chamburger");
+
+  for (var i = toggles.length - 1; i >= 0; i--) {
+    var toggle = toggles[i];
+    toggleHandler(toggle);
+  };
+
+  function toggleHandler(toggle) {
+    toggle.addEventListener( "click", function(e) {
+      e.preventDefault();
+      (this.classList.contains("is-active") === true) ? this.classList.remove("is-active") : this.classList.add("is-active");
+    });
+  }
+
+})();
 var lastId,
     topMenu = $("#cmenu"),
     topMenuHeight = topMenu.outerHeight(),
