bcd1f0aaecfaba1cb97ef44e079427f7822e147a
Cyriac Domini Thundathil <cyriacd@outlook.com>
2016-01-13 20:54:48 -0500

    background position error

--- js/headerbg.js
+++ js/headerbg.js
@@ -1,5 +1,5 @@
 var rand = Math.floor(Math.random() * 3);
-var style = "header{background-image: url(./images/back03"+".jpg);background-size:cover;background-repeat:no-repeat;}";
+var style = "header{background-image: url(./images/back03"+".jpg);background-size:cover;background-repeat:no-repeat;background-position: center center;}";
 /*
 // header{    background-image: url(../images/back00.jpg);
 //     background-size:     cover;
